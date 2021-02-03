455e1710c37f7ad509baa70a713645de8e19557e
VernandoGames <30465911+VernandoGames@users.noreply.github.com>
2021-02-03 12:05:16 -0600

    Fix mistakes with new service worker registry

--- docs/assets/javascripts/rsw.js
+++ docs/assets/javascripts/rsw.js
@@ -1,4 +1,7 @@
 // Check compatibility for the browser we're running this in
+
+const BASE_URL = https://vernandogames.github.io/InScane-SMP
+
 if ("serviceWorker" in navigator) {
     if (navigator.serviceWorker.controller) {
       console.log("[PWA Builder] active service worker found, no need to register");
@@ -6,8 +9,8 @@ if ("serviceWorker" in navigator) {
       // Register the service worker
       console.log("[PWA Builder] Attempting to register new Service worker..");
       navigator.serviceWorker
-        .register(`${process.env.BASE_URL}pwabuilder-sw.js`, {
-          scope: `${process.env.BASE_URL}`
+        .register(`${BASE_URL}pwabuilder-sw.js`, {
+          scope: `${BASE_URL}`
         })
         .then(function (reg) {
           console.log("[PWA Builder] Service worker has been registered for scope: " + reg.scope);
